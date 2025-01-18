7f93c18e514c3b409fe79976a4a4df96494aa891
Ezequiel Bacchini <ezequielbacchini@gmail.com>
2025-01-18 17:47:35 -0300

    Prueba de Cookies

--- SitioWeb/app.py
+++ SitioWeb/app.py
@@ -1,15 +1,25 @@
 from flask import Flask
-from flask import render_template, redirect, request, Response, session
+from flask import render_template, redirect, request, Response, session, make_response
 
 app = Flask(__name__,template_folder='template')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    saved_name = request.cookies.get("saved_name")
+    return render_template('index.html', saved_name = saved_name)
 
-@app.route('/accesso-login', methods=["GET", "POST"])
-def login():
-    return render_template('index.html')
+@app.route('/save_name', methods=["POST"])
+def save_name():
+    name = request.form['name']
+    response = make_response(f"Nos vamos a acordar de tu nombre, {name}!!!")
+    response.set_cookie("saved_name", name)
+    return response
+
+@app.route('/delete_cookie')
+def delete_cookie():
+    response = make_response("Nos olvidamos de tu nombre!")
+    response.set_cookie("saved_name", "", expires=0)
+    return response
 
 if __name__ == '__main__':
     app.secret_key="_Xr60031_"
